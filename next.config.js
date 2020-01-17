505b129d2c21e73327a6a59348487d92bf0bb875
Deploy <deploy@Piesofts-Mac-mini.local>
2020-01-17 16:44:38 +0700

    Start project

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 const dotenv = require('dotenv');
+const withSass = require('@zeit/next-sass')
+const withCSS = require('@zeit/next-css')
+const path = require('path')
 dotenv.config();
-module.exports = {
+module.exports = withSass(withCSS({
+    webpack(config, options) {
+        config.resolve.alias['~'] = path.resolve(__dirname);
+        return config    
+    },
     env: {
         APP_NAME: process.env.APP_NAME
     }
-  }
+}))
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import App from './index'
 import { appWithTranslation } from '../i18n'
+import '~/index.scss'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import * as Icons from '@fortawesome/free-solid-svg-icons';
+
+const iconList = Object
+  .keys(Icons)
+  .filter(key => key !== "fas" && key !== "prefix" )
+  .map(icon => Icons[icon])
+
+library.add(...iconList)
 
 class MyApp extends React.Component {
   render() {
+    const { Component, pageProps } = this.props
     return (
       <div>
-        <App></App>
+        <Component {...pageProps}></Component>
       </div>
     )
   }
